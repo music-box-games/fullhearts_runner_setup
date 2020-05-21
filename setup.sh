20e9ea78930186fbae057e2dcd867628b718da88
WaifuForHire <thewritinginthesky@gmail.com>
2020-05-21 16:47:21 -0700

    adding deb setup file

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Starting Setup"
+# install python 3
+sudo apt install -y python3
+# install python 3 setuptools
+sudo apt install -y python3-setuptools
+# install python 3 wheel
+sudo pip3 install wheel
+# install conan
+sudo pip3 install conan
+# refresh
+source ~/.profile
+# symlink for bash
+sudo ln -s ~/.local/bin/conan /usr/bin/conan
+# add bincrafters to conan remotes
+conan remote add conan-bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+# add darcamo to conan remotes
+conan remote add darcamo-bintray https://api.bintray.com/conan/darcamo/cppsim
+echo "Setup complete"
