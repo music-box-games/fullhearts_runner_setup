1dd914ff7799fd374a6531e2d5266c7efd33428c
WaifuForHire <thewritinginthesky@gmail.com>
2020-05-21 19:57:09 -0700

    checking for existing stuff

--- setup.sh
+++ setup.sh
@@ -12,11 +12,21 @@ sudo pip3 install conan
 # refresh
 source ~/.profile
 # symlink for bash
-sudo ln -s ~/.local/bin/conan /usr/bin/conan
+sudo ln -fs ~/.local/bin/conan /usr/bin/conan
+# add remotes if they aren't already added
+touch /tmp/conanremotes.txt
+conan remote list >> /tmp/conanremotes.txt
 # add bincrafters to conan remotes
+grep "conan-bincrafters" /tmp/conanremotes.txt
+if [$?=1]
+then
 conan remote add conan-bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+fi
 # add darcamo to conan remotes
+grep "darcamo-bintray" /tmp/conanremotes.txt
+if [$?=1]
 conan remote add darcamo-bintray https://api.bintray.com/conan/darcamo/cppsim
+fi
 # set libstdc++ to a modern ABI
 conan profile update settings.compiler.libcxx=libstdc++11 default
 
