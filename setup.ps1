18ccdfc9a59a72ff67c10c5b92a583ef107c90b4
WaifuForHire <thewritinginthesky@gmail.com>
2020-05-21 17:49:44 -0700

    wrote scripts

---
+++ setup.ps1
@@ -0,0 +1,28 @@
+Write-Host "Starting Setup"
+# check if we've already run the setup
+if((Test-Path env:WBUILD_RUNNER_SETUP))
+{
+if($env:WBUILD_RUNNER_SETUP == 1)
+{
+  Write-Host "env:WBUILD_RUNNER_SETUP is set to 1, nothing to setup"
+  exit
+}
+elseif($env:WBUILD_RUNNER_SETUP == 0)
+{
+  Write-Host "env:WBUILD_RUNNER_SETUP is set to 1, re-running script"
+}
+}
+else 
+{
+  Write-Host "env:WBUILD_RUNNER_SETUP is not defined, running script"  
+}
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install python3
+pip3 install conan
+refreshenv 
+conan remote add conan-bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add darcamo-bintray  https://api.bintray.com/conan/darcamo/cppsim
+
+[System.Environment]::SetEnvironmentVariable('WBUILD_RUNNER_SETUP', '1', [System.EnvironmentVariableTarget]::User)
+Write-Host "Setup complete"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 echo "Starting Setup"
+# check if we've already done this
+if [ -z "$WBUILD_RUNNER_SETUP"]
+then
+  echo "WBUILD_RUNNER_SETUP not defined, running full script"
+else
+$wval = $WBUILD_RUNNER_SETUP
+if [$wval=0]
+then
+  echo "WBUILD_RUNNER_SETUP set to 0, re-running full script"
+elif [$wval=1]
+  echo "WBUILD_RUNNER_SETUP set to 1, nothing to be done"
+  exit 0
+fi
+fi
+
 # install python 3
 sudo apt install -y python3
 # install python 3 setuptools
@@ -16,4 +31,11 @@ sudo ln -s ~/.local/bin/conan /usr/bin/conan
 conan remote add conan-bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 # add darcamo to conan remotes
 conan remote add darcamo-bintray https://api.bintray.com/conan/darcamo/cppsim
+
+# set env variable to avoid redoing this all
+export WBUILD_RUNNER_SETUP=1>>~/.bashrc
+export WBUILD_RUNNER_SETUP=1>>~/.profile
+source ~/.bashrc
+source ~/.profile
+
 echo "Setup complete"
