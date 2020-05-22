dcd1ba14af48d47f8ebee13d2fe9f2df99811535
WaifuForHire <thewritinginthesky@gmail.com>
2020-05-21 18:36:38 -0700

    restructure if

--- setup.sh
+++ setup.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 echo "Starting Setup"
 # check if we've already done this
-if [ -z "$WBUILD_RUNNER_SETUP"]
-  then
-  echo "WBUILD_RUNNER_SETUP not defined, running full script"
-
-else
+if [ ![-z "$WBUILD_RUNNER_SETUP"]]
   $wval = $WBUILD_RUNNER_SETUP
   if [$wval=0]
     then
     echo "WBUILD_RUNNER_SETUP set to 0, re-running full script"
-
   elif [$wval=1]
     echo "WBUILD_RUNNER_SETUP set to 1, nothing to be done"
-    exit 0
-
   else
     echo "WBUILD_RUNNER_SETUP set to $WBUILD_RUNNER_SETUP, re-running full script"
-
   fi
-  
 fi
 
 # install python 3
